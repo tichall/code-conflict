cea3a82b363a8f0aa263613887d6c9a5f0d2af8c
tichall <0817frog@gmail.com>
2024-07-05 09:41:30 +0900

    feature : 회원 조회 기능 구현

--- src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
+++ src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
@@ -1,4 +1,20 @@
 package kr.sparta.practiceconflict.controller;
 
+import kr.sparta.practiceconflict.entity.Member;
+import kr.sparta.practiceconflict.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members")
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
 }
